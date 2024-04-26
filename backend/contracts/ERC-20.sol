6652fcea55a5edb575c5eb0cbbeb9efef04cb43b
Jonatan Chaverri <jonatan.chaverri@kyndryl.com>
2024-04-25 20:06:04 -0600

    add erc-20 contract

---
+++ backend/contracts/ERC-20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OTToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("OTToken", "OTTO")
+        Ownable(initialOwner)
+    {
+        transferOwnership(msg.sender); // Ownership is transferred to the deployer
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
