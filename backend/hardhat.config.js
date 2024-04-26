90a052ec326a663da3175d673bf1e40f1a3cd794
Alfredo Bonilla <info@alfredobonilla.com>
2024-04-26 00:22:17 -0600

    Update hardhat config to work with scroll sepolia

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -6,17 +6,18 @@ module.exports = {
   networks: {
     scrollsepolia: {
       url: "https://sepolia-rpc.scroll.io/",
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
-  solidity: "0.8.24",
+  solidity: "0.8.23",
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
   mocha: {
-    timeout: 40000
-  }
+    timeout: 40000,
+  },
 };
