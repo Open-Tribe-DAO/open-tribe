68fe585178178420e6e4c3b1caff4d581a9efa2e
Robert <rsft6000@gmail.com>
2024-04-27 14:12:46 -0600

    wagmi integration

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import { useAccount } from "wagmi";
+import { useAccount, useReadContract } from "wagmi";
 import { CommunityCard } from "~/components/CommunityCard";
 import { Layout } from "~/components/Layout";
 
 import { api } from "~/utils/api";
+
+import { useWriteContract } from 'wagmi'
+
+import TaskManager from '~/abi/TaskManager.json'
+import Web3 from 'web3';
 
 export default function Home() {
   //const hello = api.post.hello.useQuery({ text: "from tRPC" });
@@ -13,6 +18,37 @@ export default function Home() {
   const { mutate, status, error } = api.user.create.useMutation()
   ///hello.useQuery({ text: "from tRPC" });
   const { address, isConnected } = useAccount();
+
+  const { writeContract } = useWriteContract()
+  // const { request } = await useWriteContract({
+  //   address: '0xFEa742547a8c0d2a70606B4106c5B20736BfCeD6',
+  //   abi: TaskManager.abi,
+  //   functionName: 'mint',
+  // })
+
+  // const { config, error } = usePrepareContractWrite({
+  //   address: '0xecb504d39723b0be0e3a9aa33d646642d1051ee1',
+  //   abi:TaskManager.abi,
+  //   functionName: 'feed',
+  // })
+  // const { hash } = await writeContract(request)
+
+  // const getTokens=()=>{
+  //   writeContract({}) 
+  // }
+
+  // const { data: balance } = useReadContract({
+  //   ...TaskManager,
+  //   functionName: 'name',
+  //   args: ['0xFEa742547a8c0d2a70606B4106c5B20736BfCeD6'],
+  // })
+
+  // const web3 = new Web3(window?.ethereum);
+
+  // const contract = new web3.eth.Contract(
+  //   TaskManager.abi,
+  //   "0xFEa742547a8c0d2a70606B4106c5B20736BfCeD6"
+  // );
 
   return (
     <>
@@ -28,10 +64,32 @@ export default function Home() {
               Open Tribe
             </h1>
 
+            <button
+              onClick={() => {
+                writeContract({
+                  TaskManager,
+                  address: '0xFEa742547a8c0d2a70606B4106c5B20736BfCeD6',
+                  functionName: 'mint',
+                  args: [
+                    '0xc1d457128dEcAE1CC092728262469Ee796F1Ac45',
+                    '100000000000000',
+                  ],
+                })
+
+                //contract.methods.mint.call()
+
+
+                console.log('write');
+
+              }}
+            >
+              Transfer
+            </button>
+
             <div className="flex flex-col gap-2 text-white w-full mt-[100px]">
               <h2 className="text-2xl">Communities</h2>
               {communities && communities?.map((item) => {
-                
+
                 return (
                   <CommunityCard key={item.id} id={item.id} name={item.name ?? ''} />
                 )
