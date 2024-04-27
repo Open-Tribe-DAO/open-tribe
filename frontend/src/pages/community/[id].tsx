72e20f7fa592c684818a3979159e27780f6c1eff
Randall Valenciano <ravf.226@gmail.com>
2024-04-27 15:56:15 -0600

    tasks: adding community hardcoded image

--- frontend/src/pages/community/[id].tsx
+++ frontend/src/pages/community/[id].tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router';
 import { useContext, useEffect, useState } from 'react';
 import { Layout } from '~/components/Layout';
 import { api } from "~/utils/api";
+import Image from 'next/image';
+import communityLogo from './../../../public/eth.png';
 
 export default function TicketDetailsPage() {
   const router = useRouter();
@@ -18,12 +20,16 @@ export default function TicketDetailsPage() {
       <div className="px-[10px] mt-[30px] text-white">
         <h1 className='text-3xl'>{community?.name}</h1>
         <p className='text-lg'>{community?.description}</p>
-
+        <Image 
+        src={communityLogo}
+        alt={'Community Logo'}
+        width={500}
+        height={500} />
         {community?.owner && <p>Owner: {community?.owner}</p>}
 
 
         <div className='mt-[20px]'>
-          <h2 className='text-2xl'>Tasks</h2>
+        <h2 className='text-2xl'>Tasks</h2>
 
         </div>
       </div>
