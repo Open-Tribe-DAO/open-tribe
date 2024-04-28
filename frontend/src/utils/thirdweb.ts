08993edba262a9d25fc92941324c9f1f05f50d7a
Robert <rsft6000@gmail.com>
2024-04-27 19:20:59 -0600

    create task progress

--- frontend/src/utils/thirdweb.ts
+++ frontend/src/utils/thirdweb.ts
@@ -1,3 +1,11 @@
-import { createThirdwebClient } from "thirdweb";
+import { createThirdwebClient, defineChain, getContract } from "thirdweb";
+import TaskManagerABI from "~/abi/TaskManager";
 
 export const thirdwebClient = createThirdwebClient({ clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID ?? '' });
+
+export const taskManagerContract = getContract({
+  client: thirdwebClient,
+  chain: defineChain(534351), 
+  address: "0x1B2539b195aF04f4EAb550650E588916aafA7F44",
+  abi: TaskManagerABI
+});
