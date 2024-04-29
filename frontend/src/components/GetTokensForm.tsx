400b9f3eb556ba3f4dd4c98724cd0c6afff92442
Robert <rsft6000@gmail.com>
2024-04-29 14:30:08 -0600

    chainlink get tokens data feeds

--- frontend/src/components/GetTokensForm.tsx
+++ frontend/src/components/GetTokensForm.tsx
@@ -22,7 +22,7 @@ import { useSendTransaction } from "thirdweb/react"
 const notEmpty = z.string().trim().min(1, { message: "Required" });
 
 const FormSchema = z.object({
-  tokensAmount: z.number(),
+  usdAmount: z.string(),
   communityLeader: z.string().pipe(notEmpty),
 })
 
@@ -30,11 +30,12 @@ export const GetTokensForm = ({ }) => {
   const router = useRouter();
   const [loading, setLoading] = useState(false)
   const [latestEthPrice, setLatestEthPrice] = useState(0)
+  const [usdToEthPrice, setUsdToEthPrice] = useState(0)
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      tokensAmount: 0,
+      usdAmount: '0',
       communityLeader: ""
     },
   })
@@ -42,13 +43,16 @@ export const GetTokensForm = ({ }) => {
   useEffect(() => {
     form.reset({
       ...form.getValues(), // Retains any existing form values
-      tokensAmount: 1
+      usdAmount: '1'
     });
   }, [form]);
 
   const { setValue, handleSubmit, watch, register, formState } = form;
   const { errors } = formState;
   console.log('errors', errors);
+
+  const usdAmount = watch("usdAmount");
+  const communityLeader = watch("communityLeader");
 
   const { mutate: sendTransaction, isPending } = useSendTransaction();
 
@@ -65,26 +69,46 @@ export const GetTokensForm = ({ }) => {
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log('SUBMIT', data);
     //mutate(data)
-    console.log('toWei(tokensAmount)', toWei(`${data.tokensAmount}`));
-    mintTokens(data.communityLeader, `${data.tokensAmount}`)
+    console.log('toWei(tokensAmount)', toWei(`${usdToEthPrice}`));
+    mintTokens(data.communityLeader, `${usdToEthPrice}`)
+  }
+
+  // useEffect(() => {
+  //   const getLatestEthPrice = async () => {
+  //     const ethPrice = await readContract({
+  //       contract: taskManagerContract,
+  //       method: "getLatestEthPrice",
+  //       params: [],
+  //     } as never);
+  //     console.log('ethPrice!', ethPrice);
+
+  //     setLatestEthPrice(ethPrice as any);
+  //   };
+
+  //   console.log('getLatestEthPrice', getLatestEthPrice);
+
+  //   getLatestEthPrice();
+  // }, []);
+
+  const ethPriceFormatter = (valor: number) => {
+    const divisor = 10000000000; // Esto es 10^10
+    const resultado = valor / divisor;
+    return resultado.toFixed(10); // Retorna el resultado con 10 decimales
   }
 
   useEffect(() => {
-    const getLatestEthPrice = async () => {
+    const getLatestUsdToEthPrice = async () => {
       const ethPrice = await readContract({
         contract: taskManagerContract,
-        method: "getLatestEthPrice",
-        params: [],
+        method: "usdToEth",
+        params: [usdAmount],
       } as never);
-      console.log('ethPrice', ethPrice);
-
-      setLatestEthPrice(ethPrice as any);
+      const formattedPrice = ethPriceFormatter(Number(ethPrice))
+      setUsdToEthPrice(formattedPrice as any);
     };
 
-    console.log('getLatestEthPrice', getLatestEthPrice);
-
-    getLatestEthPrice();
-  }, []);
+    getLatestUsdToEthPrice();
+  }, [usdAmount]);
 
   return (
     <div className="">
@@ -92,10 +116,10 @@ export const GetTokensForm = ({ }) => {
         <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
           <FormField
             control={form.control}
-            name="tokensAmount"
+            name="usdAmount"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="text-white">Amount* (ETH)</FormLabel>
+                <FormLabel className="text-white">Amount* (USD)</FormLabel>
                 <FormControl>
                   <Input placeholder="Amount" {...field} />
                 </FormControl>
@@ -118,19 +142,29 @@ export const GetTokensForm = ({ }) => {
             )}
           />
 
+          <div className="text-white">
+            <h3>You are sending</h3>
+            <div>
+              <p><span className="font-bold">{usdToEthPrice} OTTO</span> to <span className="font-bold">{communityLeader}</span></p>
+            </div>
+
+            <p>Powered by <a href="https://docs.chain.link/data-feeds/price-feeds/addresses" className="underline">Chainlink Data Feeds</a></p>
+          </div>
+
+
           <div className="flex justify-end">
             <Button type="submit">Get Tokens</Button>
           </div>
         </form>
       </Form>
 
-      <div className="text-white mb-[10px]">
+      {/* <div className="text-white mb-[10px]">
         <h2 className="text-2xl">Your tokens</h2>
 
         <p className="text-lg">0.2 OTTO</p>
-      </div>
+      </div> */}
 
-      <Button type="button" onClick={() => { router.push('/account') }}>Go to your account</Button>
+      {/* <Button type="button" onClick={() => { router.push('/account') }}>Go to your account</Button> */}
     </div>
   )
 }
